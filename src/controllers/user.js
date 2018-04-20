d7a290f0350ecc6c326b02e21fe029a4bb4343a9
Chong Han Khee <key@Chongs-MacBook-Pro.local>
2018-04-20 08:55:27 +0800

    Refresh

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,24 +1,37 @@
 'use strict';
+var q = require('q');
 const User = require('./../models/user');
 
 class UserController{
     signup(user){
-        let data = Object.assign(new User(), user);
-        return data.save();
-        // return User.findOne({email:user.email})
-        // .then((existUser) => {
-        //     if(existUser){
-        //         let data = Object.assign(existUser, user)
-        //     }
-        //     else{
-        //         let data = Object.assign(new User(), user)
-        //     }
-        //     return data.save()
-        // })
+        var deffered = q.defer();
+        User.findOne({email:user.email})
+        .then((existinguser) => {
+            if(existinguser){
+                deffered.reject("Record already exists.");
+            }
+            else{
+                let data = Object.assign(new User(), user);
+                data.save()
+                .then((user) => deffered.resolve(user));
+            }
+        });
+        return deffered.promise;
     }
 
-    getUser(email){
-        return User.findOne({email})
+    login(email){
+        var deffered = q.defer();
+        User.findOne({email})
+        .then((user) => {
+            if(user){
+                deffered.resolve(user);
+            }
+            else {
+                deffered.reject(`${email} : user not found.`);
+            }
+        });
+        
+        return deffered.promise;
     }
 }
 
